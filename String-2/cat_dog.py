f2207cae9e6bd481892d9d44850f15515acf5649
Rodin Opuz <opuzrodin@gmail.com>
2024-08-28 17:55:04 +0100

    Add files via upload

---
+++ String-2/cat_dog.py
@@ -0,0 +1,5 @@
+def cat_dog(str):
+  if str.count("cat") == str.count("dog"):
+    return True
+  else:
+    return False
---
+++ String-2/double_char.py
@@ -0,0 +1,5 @@
+def double_char(str):
+  result = ''
+  for i in str:
+      result += i * 2
+  return result
